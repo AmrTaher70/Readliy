09a04da2050e04e38fbb36f378aa4947a6c6f097
AmrTaher70 <amrhagiz47@gmail.com>
2024-01-20 19:21:39 +0200

    sliding text

---
+++ lib/Features/presentation/views/widget/splach_widget.dart
@@ -0,0 +1,45 @@
+import 'package:bookly/Features/presentation/views/widget/widgets/sliding_widget.dart';
+import 'package:bookly/core/assets.dart';
+import 'package:flutter/cupertino.dart';
+
+class SplashWidget extends StatefulWidget {
+  const SplashWidget({super.key});
+
+  @override
+  State<SplashWidget> createState() => _SplashWidgetState();
+}
+
+class _SplashWidgetState extends State<SplashWidget>
+    with SingleTickerProviderStateMixin {
+  //handle the time
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 15), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+}
---
+++ lib/Features/splash/splashscreen.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/Features/presentation/views/widget/splach_widget.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashWidget(),
+    );
+  }
+}
