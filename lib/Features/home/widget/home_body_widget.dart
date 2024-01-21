b21555f9b85b6c52aef434286c2d60d9b770f5dd
AmrTaher70 <amrhagiz47@gmail.com>
2024-01-21 19:05:31 +0200

    add style class and text

--- lib/Features/home/widget/home_body_widget.dart
+++ lib/Features/home/widget/home_body_widget.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/widget/custom_app_bar.dart';
 import 'package:bookly/Features/home/widget/featured_books_list_view.dart';
+import 'package:bookly/core/utils/styls.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomeWidget extends StatelessWidget {
@@ -7,11 +8,22 @@ class HomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Style.titleMedium,
+          )
+        ],
+      ),
     );
   }
 }
