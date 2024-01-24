3367cffb97833c45827fa0eab873a0c0333b642a
AmrTaher70 <amrhagiz47@gmail.com>
2024-01-24 19:14:29 +0200

    add title to book details

--- lib/Features/home/widget/book_details_view_body.dart
+++ lib/Features/home/widget/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/widget/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/widget/custom_list_view_item.dart';
+import 'package:bookly/core/utils/styls.dart';
 import 'package:flutter/cupertino.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -15,8 +16,28 @@ class BookDetailsViewBody extends StatelessWidget {
           const CustomBookDetailsAppBar(),
           //is not response to device orientation
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .35),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookItem(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Style.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Style.textStyle18.copyWith(
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
           ),
         ],
       ),
