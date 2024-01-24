053c7716e581bb994ef7dd7b8187672862e0acec
AmrTaher70 <amrhagiz47@gmail.com>
2024-01-24 19:37:28 +0200

    add reading in books details and make it page available to scroll

--- lib/Features/home/widget/book_details_view_body.dart
+++ lib/Features/home/widget/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly/Features/home/widget/custom_book_details_app_bar.dart';
 import 'package:bookly/Features/home/widget/custom_list_view_item.dart';
 import 'package:bookly/core/utils/styls.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -9,37 +10,64 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          //is not response to device orientation
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .2),
-            child: const CustomBookItem(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          Text(
-            'The Jungle Book',
-            style: Style.textStyle30.copyWith(fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Style.textStyle18.copyWith(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
+    return SingleChildScrollView(
+      physics: const AlwaysScrollableScrollPhysics(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30),
+        child: Column(
+          children: [
+            const CustomBookDetailsAppBar(),
+            //is not response to device orientation
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: width * .2),
+              child: const CustomBookItem(),
+            ),
+            const SizedBox(
+              height: 43,
+            ),
+            Text(
+              'The Jungle Book',
+              style: Style.textStyle30.copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            Opacity(
+              opacity: .7,
+              child: Text(
+                'Rudyard Kipling',
+                style: Style.textStyle18.copyWith(
+                  fontWeight: FontWeight.w500,
+                  fontStyle: FontStyle.italic,
+                ),
               ),
             ),
-          ),
-        ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  FontAwesomeIcons.solidStar,
+                  color: Color(0xffFFDD4f),
+                ),
+                const SizedBox(
+                  width: 6.3,
+                ),
+                const Text(
+                  '4.8',
+                  style: Style.textStyle16,
+                ),
+                const SizedBox(
+                  width: 6.3,
+                ),
+                Text(
+                  '(2346)',
+                  style: Style.textStyle14
+                      .copyWith(color: const Color(0xff707070)),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
