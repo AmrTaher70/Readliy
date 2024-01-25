f36e0e796efcd12d2fd0023c6ecb7201d8b8030c
AmrTaher70 <amrhagiz47@gmail.com>
2024-01-25 20:46:57 +0200

    create home repo meth and failures class

--- lib/Features/home/data/repo/home_repo.dart
+++ lib/Features/home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeatureBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
 }
