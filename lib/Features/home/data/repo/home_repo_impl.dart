87504995a7b97df4c22f861a1b2aa698f1c2fc4b
AmrTaher70 <amrhagiz47@gmail.com>
2024-02-10 18:44:41 +0200

    create feature books cubit state

--- lib/Features/home/data/repo/home_repo_impl.dart
+++ lib/Features/home/data/repo/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly/Features/home/data/repo/home_repo.dart';
 import 'package:bookly/core/error/failures.dart';
 import 'package:bookly/core/utils/api/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -23,12 +24,42 @@ class HomeRepoImpl implements HomeRepo {
       return right(books);
       // Do something with the data
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
     }
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=subject:programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+      // Do something with the data
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
 }
