735bdbc2c7c8bb642329b70855e0fa759b3859ba
AmrTaher70 <amrhagiz47@gmail.com>
2024-02-10 19:04:30 +0200

    create newest books state

--- lib/Features/presentation/manger/features/feature_books/presentation/feature_books/feature_books_cubit.dart
+++ lib/Features/presentation/manger/features/feature_books/presentation/feature_books/feature_books_cubit.dart
@@ -1,9 +1,20 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/Features/home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'feature_books_state.dart';
 
 class FeatureBooksCubit extends Cubit<FeatureBooksState> {
-  FeatureBooksCubit() : super(FeatureBooksInitial());
+  FeatureBooksCubit(this.homeRepo) : super(FeatureBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeatureBooks() async {
+    emit(FeatureBooksLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+    result.fold((failure) {
+      emit(FeatureBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(FeatureBooksSuccess(books));
+    });
+  }
 }
