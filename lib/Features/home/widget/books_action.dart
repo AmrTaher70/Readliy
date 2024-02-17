6c7507f431d900ff781d2e665c705d898c99f091
AmrTaher70 <amrhagiz47@gmail.com>
2024-02-17 20:45:17 +0200

    book details done

--- lib/Features/home/widget/books_action.dart
+++ lib/Features/home/widget/books_action.dart
@@ -1,41 +1,53 @@
 import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly/core/utils/widget/custom_button.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:bookly/core/utils/widget/launch_url.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class BooksAction extends StatelessWidget {
   const BooksAction({super.key, required this.bookModel});
+
   final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Row(
         children: [
-          Expanded(
-            child: CustomButton(
-              backgroundColor: Colors.white,
-              textColor: Colors.black,
-              text: '19.99 €',
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                bottomLeft: Radius.circular(12),
-              ),
+          const Expanded(
+              child: CustomButton(
+            text: 'Free',
+            backgroundColor: Colors.white,
+            textColor: Colors.black,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              bottomLeft: Radius.circular(16),
             ),
-          ),
+          )),
           Expanded(
-            child: CustomButton(
-              backgroundColor: Color(0xffEF8262),
-              textColor: Colors.white,
-              text: 'Free Preview ',
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(12),
-                bottomRight: Radius.circular(12),
-              ),
+              child: CustomButton(
+            onPressed: () {
+              launchCustomUr(context, 'bookModel.volumeInfo.previewLink');
+            },
+            fontSize: 16,
+            text: getText(bookModel),
+            backgroundColor: const Color(0xffEF8262),
+            textColor: Colors.white,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(16),
+              bottomRight: Radius.circular(16),
             ),
-          ),
+          )),
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Avaliable';
+    } else {
+      return 'Preview';
+    }
   }
 }
--- lib/core/utils/widget/custom_button.dart
+++ lib/core/utils/widget/custom_button.dart
@@ -1,38 +1,45 @@
 import 'package:bookly/core/utils/styls.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton(
-      {super.key,
-      required this.backgroundColor,
-      required this.textColor,
-      this.borderRadius,
-      required this.text,
-      this.sizeText});
+  const CustomButton({
+    super.key,
+    required this.backgroundColor,
+    required this.textColor,
+    this.borderRadius,
+    required this.text,
+    this.fontSize,
+    this.onPressed,
+  });
+  final String text;
   final Color backgroundColor;
   final Color textColor;
   final BorderRadius? borderRadius;
-  final String text;
-  final double? sizeText;
-
+  final double? fontSize;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 48,
       child: TextButton(
-        onPressed: () {},
+        onPressed: onPressed,
         style: TextButton.styleFrom(
-            backgroundColor: backgroundColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: borderRadius ??
-                  BorderRadius.circular(
-                    12,
-                  ),
-            )),
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadius ??
+                BorderRadius.circular(
+                  12,
+                ),
+          ),
+        ),
         child: Text(
           text,
-          style: Style.textStyle18
-              .copyWith(color: textColor, fontWeight: FontWeight.w900),
+          style: Style.textStyle18.copyWith(
+            color: textColor,
+            fontWeight: FontWeight.w900,
+            fontSize: fontSize,
+          ),
         ),
       ),
     );
